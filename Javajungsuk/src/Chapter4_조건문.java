a8a262ae92b0340f63d831420cf97af881ffaba5
mylazysundaycat <mylazysundaycat@gmail.com>
2023-02-07 11:51:19 +0900

    2023/02/07

--- Javajungsuk/src/Chapter4_조건문.java
+++ Javajungsuk/src/Chapter4_조건문.java
@@ -8,7 +8,7 @@ public class Chapter4_조건문 {
 	  //학점계산기
 	  /*
 	  System.out.println("당신의 중간고사 점수를 입력하세요 : ");
-	  Scanner sc = new Scanner(System.in);
+	  // Scanner sc = new Scanner(System.in);
 	  score = sc.nextInt();
 	  
 	  if(score >= 90) {
@@ -71,8 +71,79 @@ public class Chapter4_조건문 {
 		  i++;
 	  }
 	  
-	  //
+	  // 116p 예제 풀어보기
 	  
+	  int a = 5;
+	  while(a--!=0) {
+		  System.out.println(a);
+	  }
+	  
+	  int b = 5;
+	  while(b!=0) {
+		  b--;
+		  System.out.println(b);
+	  }
+	  
+	  //117p 예제 풀어보기
+	  //각 자리수의 합을 알려주는 프로그램을 만들어보자.
+	  
+	  /*
+	  int result = 0;
+	  
+	  System.out.println("숫자를 입력하시오 : ");
+	  Scanner sc = new Scanner(System.in);
+	  String input  = sc.nextLine();
+	  int num = Integer.parseInt(input);
+	  
+	  
+	  while(num!=0) {
+		  result += num%10;
+		  num = (num - num%10)/10; // num/10 을 넣어도 몫이여서 같은값이 된다.
+	  }
+	  
+	  System.out.printf("각 자리수의 합입니다:%d", result);
+	  */
+	  
+	  
+		/* 
+		 * do- while문 
+		 * 기존의 while문은 조건식에 따라 {}블럭이 실행될지 안될지를 결정한다면 do-while문은 조건식과는 관계없이
+		 * 선행된 {}블럭을 1번은 실행하고나서 조건식을 통해 그 다음에 {}블럭이 실행될지 안될지를 평가한다.
+		 */
+	  
+	  //p118 랜덤한 숫자를 입력해서 맞추는 프로그램 만들어보기
+	  
+
+	  Scanner sc2 = new Scanner(System.in);
+	  int correctNum = (int)(Math.random()*100)+1; // 1<= x <101 의 정수값 랜덤배정
+	  int num2 = 0;
+	  
+	  do {
+		  System.out.println("숫자를 맞춰보세요 : ");
+		  String input2 = sc2.nextLine();
+		  num2 = Integer.parseInt(input2);
+		  
+		  if(num2==correctNum) {
+			  System.out.println("정답입니다! 게임을 마칩니다! ");
+		  }
+		  else if(num2>correctNum){
+			  System.out.println("정답 숫자는 더 작을겁니다.");
+		  }
+		  else if(num2<correctNum) {
+			  System.out.println("정답 숫자는 더 클겁니다.");
+		  }
+	  } while (correctNum!=num2);
+
+
+	  /*
+	   break
+	   자신이 포함된 가장 가까운 반복문(for, while)에서 강제로 벗어나게 해준다
+	   
+	   continue
+	   반목문 전체를 벗어나지 않게 하고 해당 명령문을 만나면 다음 반복으로 수행하게 해준다.
+	   
+	   for(;;) << 한무 반복문인듯
+	   */
 	  
 	  
 	}
---
+++ Javajungsuk/src/Chapter5_배열.java
@@ -0,0 +1,121 @@
+import java.util.Arrays;
+
+public class Chapter5_배열 {
+
+	public static void main(String[] args) {
+		
+		/*
+		배열의 요소: 생성된 배열의 각 저장공간
+		배열이름[인덱스]의 형식으로 요소에 접근한다.
+		배열.length : 배열의 길이를 알려줌 -> 상수값이라 변경 불가능8
+		 */
+		int[] num = new int[5]; // 배열길이가 5인 배열 생성
+		int[] num2 = new int[] {50,60,70,80};
+		int[] num3 = {10,20,30};
+		
+		for(int i=0; i<num.length; i++) {
+			num[i]=i+1;
+			System.out.println(num[i]);
+		}
+		
+		for(int i=0; i<num3.length; i++) {
+			System.out.println(num3[i]);
+		}
+		
+		System.out.println(num3);
+		System.out.println(Arrays.toString(num3));
+		
+		/*
+		 배열의 전체 요소를 돌려받는 방법
+		 Arrays.toString(num3)
+		 */
+		
+		//137p 총합과 평균
+		
+		int[] num4 = {2,4,7,136,12,55,92};
+		int sum = 0;
+		
+		for(int i=0; i<num4.length; i++) {
+			sum += num4[i];
+		}
+		
+		System.out.println("배열의합계:"+sum);
+		System.out.println("배열의평균:"+sum/(float)num4.length);
+	
+		
+		//137p 최대값과 최소값
+		
+		//나의 풀이: 최댓값과 최솟값을 따로 떼어서 비교할 수 있는 빈 공간을 만듬
+		int maxNum = 0;
+
+		for(int i=0; i<num4.length; i++) {
+			if(maxNum<num4[i]) {
+				maxNum = num4[i];
+			}
+		}
+		
+		int minNum = maxNum;
+		
+		for(int i=0; i<num4.length; i++) {
+			if(minNum>num4[i]) {
+				minNum = num4[i];
+			}
+		}
+		
+		System.out.println("최댓값:"+maxNum);
+		System.out.println("최솟값:"+minNum);
+		
+		//해설지의 풀이: 배열의 첫번째 요소를 최댓값,최솟값으로 설정함
+		int maxNum2 = num4[0];
+		int minNum2 = num4[0];
+		
+		for(int i=0; i<num4.length; i++) {
+			if(maxNum2<num4[i]) {
+				maxNum2 = num4[i];
+			}
+		}
+		
+		System.out.println("최댓값:"+maxNum2);
+		
+		for(int i=0; i<num4.length; i++) {
+			if(minNum2>num4[i]) {
+				minNum2 = num4[i];
+			}
+		}
+		
+		System.out.println("최솟값:"+minNum2);
+		
+		
+		//151p 예제 구현하기
+		int[][] score = {
+				{99,99,99},
+				{80,70,60},
+				{50,90,70},
+				{70,45,55},
+				{25,10,95}
+		};
+		int scoreSum = 0;
+		float scoreAverage = 0;
+		System.out.println("번호  국어  영어  수학  평균점수");
+		System.out.println("=========================");
+		
+		for(int i=0; i<score.length; i++) {
+			System.out.printf("%-5d",i+1);
+			for(int j=0; j<score[i].length; j++) {
+				scoreSum+=score[i][j];
+				System.out.printf("%-5d",score[i][j]);
+				if(j==score[i].length-1) {
+					scoreAverage = scoreSum/score[i].length;
+					System.out.printf("%-5f",scoreAverage);
+					scoreSum=0;
+
+				}
+			}
+		}
+		
+		
+		
+
+	}
+
+}
