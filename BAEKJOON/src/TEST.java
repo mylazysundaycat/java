ada77b2c2aa840898958947e035bc3a253e5b608
mylazysundaycat <mylazysundaycat@gmail.com>
2023-03-02 20:51:47 +0900

    2023/03/02 백준코테_사칙연산

---
+++ BAEKJOON/src/TEST.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TEST {
+	public static void main(String[] args) {
+		/*
+		Scanner sc = new Scanner(System.in);
+		String input[] = sc.nextLine().split(" ");
+		int sum = 0;
+		for(int i = 0; i<input.length; i++) {
+			sum += Integer.parseInt(input[i]);
+		}
+		System.out.println(sum);
+		*/
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println(a+b);
+	}
+}
---
+++ BAEKJOON/src/code_10998.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class code_10998 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextInt()*sc.nextInt());
+	}
+}
